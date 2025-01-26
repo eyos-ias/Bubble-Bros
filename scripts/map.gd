c07b7f7cbfbb7ce2c4fb413f3e46dceba9757a66
Eyosias <75771087+eyos-ias@users.noreply.github.com>
2025-01-26 14:48:10 +0300

    kinda bugg but aight

--- scripts/map.gd
+++ scripts/map.gd
@@ -13,8 +13,10 @@ var enet_peer = ENetMultiplayerPeer.new()
 const player_scene = preload("res://addons/fpc/character.tscn")
 @onready var canvas_layer = $CanvasLayer
 var is_client: bool = false
+var ip_address: String
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	ip_address = IpAddress.ip_address
 	pass # Replace with function body.
 
 
@@ -26,7 +28,9 @@ func _process(delta: float) -> void:
 func _on_join_pressed() -> void:
 	is_client = true
 	canvas_layer.visible = false
-	enet_peer.create_client("192.168.0.104", 135)
+	# enet_peer.create_client("192.168.0.104", 135)
+	print("joining ip")
+	enet_peer.create_client(ip_address, 135)
 	multiplayer.multiplayer_peer = enet_peer
 	add_player(multiplayer.get_unique_id())
 
